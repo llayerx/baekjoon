d2a4dea346b86021db7fc17b8fd5af8a6f4d65fe
jhlee <leejinha829@gmail.com>
2022-08-01 17:27:22 +0900

    22.08.01 update

--- done/2581_소수.py
+++ done/2581_소수.py
@@ -1,27 +1,5 @@
 # https://www.acmicpc.net/problem/2581
-<<<<<<< HEAD
 
-def isPrimeList(lst):
-    tmp = lst.copy()
-    m, n = lst[0], lst[-1]
-
-    if n < 3:  # 1->1 2->0
-        tmp = [k % 2 for k in lst]
-
-    lst = [k % 2 for k in lst]  # even -> 0
-    d = 3
-    while d * d <= n:
-        lst = [(1 if k % d == 0 else k) for k in lst]  # 0 if not prime
-        d += 2
-    return lst
-
-m, n = map(int, input().split())
-
-nlst = [k for k in range(m, n + 1)]
-print(nlst)
-print(isPrimeList(nlst))
-
-=======
 import math
 
 def isPrime(n):
@@ -71,4 +49,3 @@ else:
 #     print(min(tmp))
 # else:
 #     print('-1')
->>>>>>> origin/main
