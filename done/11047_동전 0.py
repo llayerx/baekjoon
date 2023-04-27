79a0506b1ca119e9ac1f31b1b71c700ac57246e6
jhlee <leejinha829@gmail.com>
2023-04-27 09:47:42 +0900

    update 230427

---
+++ done/11047_동전 0.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open('input.txt')
+
+all = sys.stdin.readlines()
+_, sum = map(int, all[0][:-1].split(' '))
+coins = list(map(int, all[1:]))[::-1]
+
+count = 0 
+for coin in coins:
+    n, l = divmod(sum, coin)
+    count += n
+    sum = l
+print(count)
